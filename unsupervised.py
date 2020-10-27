a6261e1a7f8805db2d60b6162aeacf429fe70c03
shuhei <shukuri.7336.8@gmail.com>
2020-10-28 04:53:23 +0900

    debug

--- unsupervised.py
+++ unsupervised.py
@@ -149,7 +149,7 @@ if params.adversarial:
 
         # update the learning rate (stop if too small)
         trainer.update_lr(to_log, VALIDATION_METRIC)
-        if trainer.map_optimizer.param_groups[0]['lr'] < params.min_lr:
+        if trainer.gen_optimizer.param_groups[0]['lr'] < params.min_lr:
             logger.info('Learning rate < 1e-6. BREAK.')
             break
 
