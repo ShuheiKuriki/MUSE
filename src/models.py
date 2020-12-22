15c48a07226043b58eaf57c830c5d01d5068c58f
shuhei <shukuri.7336.8@gmail.com>
2020-12-22 10:15:01 +0900

    add emb pattern

--- src/models.py
+++ src/models.py
@@ -88,9 +88,14 @@ class Embedding(nn.Module):
                 _embs[-1] = torch.randn(params.random_vocab, params.emb_dim) / (params.emb_dim**.5)
                 if params.emb_init == 'norm_mean':
                     norm_mean = torch.mean(torch.cat([torch.norm(_embs[l], dim=1, keepdim=True).expand_as(_embs[l]) for l in range(params.langnum-1)]).view(params.langnum-1, -1, params.emb_dim), dim=0)
-                    print(norm_mean)
-                    _embs[-1] *= norm_mean[:params.random_vocab] / torch.mean(torch.norm(norm_mean[:params.random_vocab], dim=1))
-                _embs[-1] *= params.emb_norm
+                    _embs[-1] *= norm_mean[:params.random_vocab] / torch.mean(norm_mean[:params.random_vocab]) * params.emb_norm
+                elif params.emb_init == 'en_mean':
+                    norm_mean = torch.norm(_embs[0], dim=1, keepdim=True).expand_as(_embs[0])
+                    _embs[-1] *= norm_mean[:params.random_vocab]
+                elif params.emb_init == 'lang_mean':
+                    _embs[-1] = torch.mean(torch.cat([_embs[l] for l in range(params.langnum-1)]).view(params.langnum-1, -1, params.emb_dim), dim=0)
+                else:
+                    _embs[-1] *= params.emb_norm
             else:
                 dicos[i], _embs[i] = load_embeddings(params, i)
         self.embs = nn.ModuleList([nn.Embedding(len(dicos[i]), params.emb_dim, sparse=False) for i in range(self.langnum)])
