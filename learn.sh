a6627fd3fa2519cc8627b7d53b6dcd96f84fc9e9
shuhei <shukuri.7336.8@gmail.com>
2021-01-02 15:52:24 +0900

    delete shell

--- learn.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for ((j=3; j<8; j=j+2))
-do
-  python learn_embedding_only.py --exp_id norm_mean3.9_lr${j}_p.3 --emb_norm 3.9 --emb_lr ${j} --dis_sampling 0.3 --exp_name learn_emb_only --device cuda:3 --langs en_es_random --emb_init norm_mean --n_epochs 50
-done
