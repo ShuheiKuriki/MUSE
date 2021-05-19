6ca106817788b9756d2035fac8e262841143ba27
shuhei <shukuri.7336.8@gmail.com>
2021-05-19 23:56:40 +0900

    update some files

---
+++ data/align_europarl.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+
+# TOOLS=tools
+# mkdir -p $TOOLS
+
+# cd $TOOLS
+
+fast_align
+git clone https://github.com/clab/fast_align
+cd fast_align
+mkdir build
+cd build
+cmake ..
+make
+cd ..
+
+set -e
+
+mkdir -p cache
+
+for L1 in en
+do
+    for L2 in bg el de es fr
+    do
+        MODEL=bert-base-multilingual-cased
+        FAST_ALIGN=./tools/fast_align/build
+        OUTPUT_PATH=./crosslingual/europarl
+        OUTPUT_PATH2=./crosslingual/aligned_europarl/$L2-$L1
+        mkdir $OUTPUT_PATH2
+        CACHE=./cache
+        PARA=europarl-v7.$L1-$L2.30k
+
+        # download and prep parallel data
+        wget https://www.statmt.org/europarl/v7/$L2-$L1.tgz
+        tar -zxvf $L2-$L1.tgz -C $OUTPUT_PATH/
+        rm -f $L2-$L1.tgz
+        python prep_parallel.py --bert_model $MODEL --lang1 $OUTPUT_PATH/europarl-v7.$L2-$L1.$L1 --lang2 $OUTPUT_PATH/europarl-v7.$L2-$L1.$L2 --size 30000 --output $OUTPUT_PATH2/$PARA
+
+        # get word alignment
+        $FAST_ALIGN/fast_align -i $OUTPUT_PATH2/$PARA.uncased -d -o -v > $OUTPUT_PATH2/forward.align.$PARA.uncased
+        $FAST_ALIGN/fast_align -i $OUTPUT_PATH2/$PARA.uncased -d -o -v -r > $OUTPUT_PATH2/reverse.align.$PARA.uncased
+        $FAST_ALIGN/atools -i $OUTPUT_PATH2/forward.align.$PARA.uncased -j $OUTPUT_PATH2/reverse.align.$PARA.uncased -c grow-diag-final-and > $OUTPUT_PATH2/sym.align.$PARA.uncased
+    done
+done
--- src/orgnize_dicts.py
+++ src/orgnize_dicts.py
@@ -2,8 +2,8 @@
 import os
 fr = '../embeddings/filtered_dictionaries'
 to = 'data/crosslingual/dictionaries'
-langs = ['fra', 'hin', 'kor', 'rus', 'ita', 'por', 'spa', 'zho', 'jpn']
-langs2 = ['fr', 'hi', 'ko', 'ru', 'it', 'pt', 'es', 'zh', 'ja']
+langs = ['hin', 'kor', 'rus', 'fin', 'jpn']
+langs2 = ['hi', 'ko', 'ru', 'fi', 'ja']
 for l11, l12 in zip(langs, langs2):
     for l21, l22 in zip(langs, langs2):
         if l11 == l21: continue
@@ -12,9 +12,12 @@ for l11, l12 in zip(langs, langs2):
         with open(path) as f:
             lines = f.readlines()
             for line in lines:
-                a = line.split()[0]
-                b = line.split()[1]
-                dicts.append('{} {}'.format(a, b))
+                try:
+                    a = line.split()[0]
+                    b = line.split()[1]
+                    dicts.append('{} {}'.format(a, b))
+                except:
+                    pass
         path = os.path.join(to, '{}-{}.0-5000.txt'.format(l12, l22))
         with open(path, 'w') as f:
             f.write('\n'.join(dicts))
@@ -24,9 +27,12 @@ for l11, l12 in zip(langs, langs2):
         with open(path) as f:
             lines = f.readlines()
             for line in lines:
-                a = line.split()[0]
-                b = line.split()[1]
-                dicts.append('{} {}'.format(a, b))
+                try:
+                    a = line.split()[0]
+                    b = line.split()[1]
+                    dicts.append('{} {}'.format(a, b))
+                except:
+                    pass
         path = os.path.join(to, '{}-{}.5000-6500.txt'.format(l12, l22))
         with open(path, 'w') as f:
             f.write('\n'.join(dicts))
